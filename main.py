08d0b798307fee2457f007b78465b24f9cc010a6
jholkeboer <jholkeboer@users.noreply.github.com>
2016-07-06 20:24:46 -0700

    bfs crawl function started

--- main.py
+++ main.py
@@ -6,6 +6,37 @@ import lxml
 from lxml import html
 app = Flask(__name__)
 
+def getPageLinks(parentURL):
+    res = urlfetch.fetch(parentURL)
+    tree = lxml.html.fromstring(res.content)
+
+    links = []
+    for node in tree.iter():
+        if node.tag == 'a':
+            link = node.get('href')
+            if link and link.startswith('http'):
+                nextLink = {'parent': parentURL, 'child': link}
+                print nextLink
+                links.append(nextLink)
+    return links
+
+def breadthFirstCrawl(startingURL, recursionLimit, currentLevel):
+    print "Level" + str(currentLevel)
+    
+    # see here: http://stackoverflow.com/questions/9762685/using-the-requests-python-library-in-google-app-engine
+    # get html tree of starting page
+    res = urlfetch.fetch(startingURL)
+    tree = lxml.html.fromstring(res.content)
+    
+    results = getPageLinks(startingURL)
+    if currentLevel < recursionLimit:
+        current_level_result = results
+        for link in current_level_result:
+            results.extend(breadthFirstCrawl(link['child'], recursionLimit, currentLevel + 1))
+    else:
+        return results
+
+    return results
 
 @app.route('/')
 def index():
@@ -21,19 +52,15 @@ def crawl():
     print recursionLimit
     print searchType
 
-    # see here: http://stackoverflow.com/questions/9762685/using-the-requests-python-library-in-google-app-engine
-    res = urlfetch.fetch(startingURL)
-    tree = lxml.html.fromstring(res.content)
+    
+    if searchType == 'bfs':
+        result = breadthFirstCrawl(startingURL, recursionLimit, 0)
+    # elif searchType == 'dfs':
+    #     result = ""
+    else:
+        result = []
 
-    links = []
-    for node in tree.iter():
-        if node.tag == 'a':
-            link = node.get('href')
-            if link and link.startswith('http'):
-                print link
-                links.append(link)
-
-    return json.dumps({'status': 'Ok', 'count': len(links)})
+    return json.dumps({'status': 'Ok', 'count': len(result)})
 
 
 @app.errorhandler(404)
