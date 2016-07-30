12e3a020e3394008d409a75fe514bf41616b594b
Patrick Ho <patho@Patricks-MBP.attlocal.net>
2016-07-30 01:08:05 -0700

    zooming features

--- static/main.js
+++ static/main.js
@@ -13,36 +13,76 @@
 		}
 	}
 
-function htmlCode(level, totalLevels){
-	// htmlCode(level, totalLevels) where level is 0 indexed and totalLevels is a count
-	// The following example covers all color levels
-	/*
-	console.log(htmlCode(0,4));
-	console.log(htmlCode(1,4));
-	console.log(htmlCode(2,4));
-	console.log(htmlCode(3,4));
-	*/
-	var p = (level / (totalLevels - 1)).toPrecision(3);
-	if ( (level / (totalLevels-1)).toPrecision(6) <= 0.5){
-		var c = Math.floor(255 * p / 0.5);
-		c = (c == 0) ? "00" : c;
-		return (255).toString(16) + "00" + c.toString(16);
-	} else{
-		var c = Math.floor(255-(p - 0.5)/0.5 * 255).toString(16);
-		c = (c == 0) ? "00" : c;
-		return c +"00"+ (255).toString(16);
+	 var crawlerResultsSample2 = {
+	 	nodes:{
+				google:{link:"http://www.google.com",level:0},
+				yahoo:{link:"http://www.yahoo.com",level:1},
+				},
+		edges:{
+				google:{yahoo:{}},
+		}
 	}
+
+function nodeColors(levels){
+	var partition = (510 / levels).toPrecision(3);
+	var colors = []
+	for (var i = 0 ; i < levels ; i++){
+		var n = partition * i;
+		var r = (n <= 255) ? 255 : 255 - n % 255;
+		if (n == 510) r = 0;
+		var b = (n <= 255) ? n : 255; 
+		colors.push({"r":r,"g":0,"b":b});
+	}
+	return colors;
 }
 
 (function($){
+
+	var included;
+	var excluded;
+ 	var showObj = function(ps,obj,numEdges){
+		var c = 0;
+		included = {nodes:{},edges:{}};
+		excluded = {nodes:{},edges:{}};
+		$.map(obj.edges, function(val,i){
+			if (c < numEdges){
+				included.edges[i] = {};
+				included.nodes[i] = JSON.parse(JSON.stringify(obj.nodes[i]));	
+			} else {
+				excluded.edges[i] = {};
+				excluded.nodes[i] = JSON.parse(JSON.stringify(obj.nodes[i]));
+			}
+			for (var property in val){
+				if (val.hasOwnProperty(property)){
+					if (c < numEdges){
+						included.edges[i][property] = {};
+						included.nodes[property] = JSON.parse(JSON.stringify(obj.nodes[property]));
+					} else {
+						if (typeof excluded.edges[i] == "undefined"){
+							excluded.edges[i] = {};
+							excluded.nodes[i] = JSON.parse(JSON.stringify(obj.nodes[i]));
+						}
+						excluded.edges[i][property] = {};
+						excluded.nodes[property] = JSON.parse(JSON.stringify(obj.nodes[property]));
+					}
+					c += 1;
+				}
+			}
+		});			
+		/*
+		console.log("included:");
+		console.log(included);
+		console.log("excluded: ");
+		console.log(excluded);
+		*/
+		ps.data.merge(included);
+		return [included,excluded];
+	}
 
  	var clearCanvas = function(ps){
 		ps.data.eachNode(function(node,pt){
 			ps.data.pruneNode(node);
 		});
-		//var canvas = $("#viewport").get(0);
-		//var ctx = canvas.getContext("2d");
-		//ctx.clearRect(0,0,canvas.width,canvas.height);
 	}
 
 	var submitCrawl = function(ps){
@@ -60,17 +100,19 @@ function htmlCode(level, totalLevels){
 					"searchType": searchType},
 			success: function(result) {
 				var crawlerResults = JSON.parse(result).result;
-				console.log(crawlerResults)
-				ps.data.graft(crawlerResults)
+				var ds = showObj(ps,crawlerResultsSample,3);
+				included = ds[0];
+				excluded = ds[1];
 			}
 		})
 	}
 
   var Renderer = function(canvas){
+    var particleSystem
     var dom = $(canvas)
     var canvas = $(canvas).get(0)
     var ctx = canvas.getContext("2d")
-    var particleSystem
+	 var depth = parseInt(document.getElementById("recursion-limit").value) + 1;
 
 	 var that = {
       init:function(system){
@@ -87,11 +129,12 @@ function htmlCode(level, totalLevels){
         // the new dimensions
         particleSystem.screenSize(canvas.width, canvas.height) 
         particleSystem.screenPadding(80) // leave an extra 80px of whitespace per side
+
         
         // set up some event handlers to allow for node-dragging
         that.initMouseHandling()
       },
-      
+     
       redraw:function(){
         // 
         // redraw will be called repeatedly during the run whenever the node positions
@@ -104,15 +147,15 @@ function htmlCode(level, totalLevels){
         // 
         ctx.fillStyle = "white"
         ctx.fillRect(0,0, canvas.width, canvas.height)
-        
+
         particleSystem.eachEdge(function(edge, pt1, pt2){
           // edge: {source:Node, target:Node, length:#, data:{}}
           // pt1:  {x:#, y:#}  source position in screen coords
           // pt2:  {x:#, y:#}  target position in screen coords
 
           // draw a line from pt1 to pt2
-          ctx.strokeStyle = "rgba(0,0,0, .333)"
-          ctx.lineWidth = 0.25
+          ctx.strokeStyle = "rgba(0,0,0, .1)"
+          ctx.lineWidth = 0.2
           ctx.beginPath();
           ctx.moveTo(pt1.x, pt1.y);
           ctx.lineTo(pt2.x, pt2.y);
@@ -124,30 +167,45 @@ function htmlCode(level, totalLevels){
         particleSystem.eachNode(function(node, pt){
           // node: {mass:#, p:{x,y}, name:"", data:{}}
           // pt:   {x:#, y:#}  node position in screen coords
+
+			//ctx.fillStyle = "#" + htmlCode(node.data.level,2);
+			node.fixed = true;
 			
-			
+			var c = nodeColors(depth);
+			var l = parseInt(node.data.level)-1;
+			/*
+			if (50 + 500 * (l / depth) > 0)	
+				node.p.y = 50 + 500 * (l / depth)
+			*/
+			ctx.fillStyle = "grey";
+
+			/*
+			if (l){
+				ctx.fillStyle = "rgba(" + c[l].r + "," + c[l].g + "," + c[l].b +",.333)";
+			} else {
+				ctx.fillStyle = "grey";
+			}*/
+			ctx.beginPath();
 			ctx.arc(pt.x,pt.y,2,0,2.0* Math.PI);
 			ctx.closePath();
-			ctx.fillStyle = "grey";
 			ctx.fill();
-			/*
-		    var w = ctx.measureText(node.data.name||"").width+20
-			 var label = node.name
-			 ctx.clearRect(pt.x-w/2,pt.y-7,w,20)
-			 ctx.font = "bold 11px Consolas"
-			 ctx.textAlign = "center"
+			 //var w = ctx.measureText(node.data.name||"").width+20
+			 //var label = node.name
+			 //ctx.clearRect(pt.x-w/2,pt.y-7,w,20)
+			 //ctx.font = "bold 11px Consolas"
+			 //ctx.textAlign = "center"
 			 //ctx.fillStyle = "#" + htmlCode(node.data.level,4);
-			 ctx.fillStyle = "grey";
-			 ctx.fillText(label||"",pt.x,pt.y+4)
-			 */
-        })    			
+			 //ctx.fillStyle = "grey";
+			 //ctx.fillText(label||"",pt.x,pt.y+4)
+		  })    			
       },
       
       initMouseHandling:function(){
         // no-nonsense drag and drop (thanks springy.js)
         var dragged = null;
 		  var nearest = null;
-
+		  var scrollUp = 0;
+		  var scrollDown = 0;
         // set up a handler object that will initially listen for mousedowns then
         // for moves and mouseups while dragging
         var handler = {
@@ -156,11 +214,8 @@ function htmlCode(level, totalLevels){
             var pos = $(canvas).offset();
             _mouseP = arbor.Point(e.pageX-pos.left, e.pageY-pos.top)
 				nearest = particleSystem.nearest(_mouseP);
-            //nearest = dragged = particleSystem.nearest(_mouseP);
 				selected = (nearest.distance < 50) ? nearest : null
             if (selected && selected.node !== null){
-					/*particleSystem.addNode("link1","link2");
-					particleSystem.addEdge(selected.node,"link1");*/
 					window.open(selected.node.data.link,'_blank');
             }
             return false
@@ -168,50 +223,51 @@ function htmlCode(level, totalLevels){
 
 			 mouseover:function(e){
 				var lbl = $('#mouseover-link')
-				lbl.text("Traces: ")
+				lbl.text("Site: ")
             var pos = $(canvas).offset();
             _mouseP = arbor.Point(e.pageX-pos.left, e.pageY-pos.top)
             nearest = dragged = particleSystem.nearest(_mouseP);
 				nearest = (nearest.distance < 50) ? nearest : null
+
 				if (nearest && nearest.node != null){
 					var link = document.createElement("a")
 					link.text = nearest.node.data.link
 					link.href = nearest.node.data.link
 					lbl.append(link)
 				}
-
 				return false
 		    },
 
-          dragged:function(e){
-            var pos = $(canvas).offset();
-            var s = arbor.Point(e.pageX-pos.left, e.pageY-pos.top)
+			 scroll:function(e){
+			 	if(e.originalEvent.detail > 0){
+					scrollDown += 1;
+					if (scrollDown == 5){
+						scrollDown = 0;
+						increaseNodes();
 
-            if (dragged && dragged.node !== null){
-              var p = particleSystem.fromScreen(s)
-              dragged.node.p = p
-            }
+					}
+				} else {
+					scrollUp += 1;
+					if (scrollUp == 5){
+						scrollUp = 0;
+						decreaseNodes();
+					}
+				}
+				return false;
+		  	 },
 
-            return false
-          },
-
-          dropped:function(e){
-            if (dragged===null || dragged.node===undefined) return
-            if (dragged.node !== null) dragged.node.fixed = false
-            dragged.node.tempMass = 1000
-            dragged = null
-				
-            $(canvas).unbind('mousemove', handler.dragged)
-            $(window).unbind('mouseup', handler.dropped)
-				
-            _mouseP = null
-            return false
-          }
+			 preventCanvasWindow:function(e){
+				e.preventDefault();
+				return false;
+			 },
         }
         
         // start listening
         $(canvas).mousedown(handler.clicked);
 		  $(canvas).mousemove(handler.mouseover);
+		  $(canvas).bind("mousewheel",handler.scroll);
+		  $(canvas).bind("DOMMouseScroll",handler.scroll);
+		  $(canvas).bind("mouseover",handler.preventWindowScroll);
 
       },
       
@@ -220,8 +276,8 @@ function htmlCode(level, totalLevels){
   }    
 
   $(document).ready(function(){
-//    var sys = arbor.ParticleSystem(1000, 600, 0.5) // ( repulsion/stiffness/friction )
-		var sys = arbor.ParticleSystem(1, 1, 1) // ( repulsion/stiffness/friction )
+//		var sys = arbor.ParticleSystem({friction:0.2, stiffness:250, repulsion:10, fps:100, dt:0.01, precision: 0.1})
+		var sys = arbor.ParticleSystem({friction:0.5, stiffness:25, repulsion:10, fps:100, dt:0.01, precision: 0.1})
     sys.parameters({gravity:false}) // use center-gravity to make the graph settle nicely (ymmv)
     sys.renderer = Renderer("#viewport") // our newly created renderer will have its .init() method called shortly by sys...
 	 $("#submit-crawl").click(sys, submitCrawl);
@@ -229,4 +285,3 @@ function htmlCode(level, totalLevels){
   })
 
 })(this.jQuery)
-
